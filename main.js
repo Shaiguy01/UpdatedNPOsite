be4fa7c0f41b742942ecc86ef2e9046c8815a788
Shahil Jagjivan <shahiljagjivan@gmail.com>
2024-02-12 10:32:05 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scroll = new SmoothScroll('a[href^="#"]', {
+        speed: 800, // Adjust the scrolling speed as needed
+        easing: 'easeInOutCubic', // You can experiment with different easing functions
+        offset: 90 // Adjust this value to set the offset
+    });
+});
+
+//navbar stuff
+const navLinks = navMenu.find('a');
+navLinks.on("click", () => {
+    // Close the mobile menu
+    navMenu.css("right", "-100%");
+});
